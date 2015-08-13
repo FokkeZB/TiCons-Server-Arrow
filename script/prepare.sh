5f0d71b4b7dce447a08a12f071640b1078c933e7
Fokke Zandbergen <mail@fokkezb.nl>
2015-08-13 16:58:08 +0200

    Add preinstall script for zip

---
+++ script/prepare.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "---"
+echo "Start installing some tools..."
+echo "(This bash script is run at npm preinstall.)"
+echo "---"
+apt-get install -qq zip
+echo "---"
+echo "done installing additional tools!"
+echo "---"
