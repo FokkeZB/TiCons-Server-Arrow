3d16209e227a6751da87ae6308296b7e3921bf97
Fokke Zandbergen <mail@fokkezb.nl>
2015-10-05 17:04:06 +0200

    add apt-get update, no result

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ echo "---"
 echo "Start installing some tools..."
 echo "(This bash script is run at npm preinstall.)"
 echo "---"
+apt-get update
 apt-get install -qq zip
 echo "---"
 echo "done installing additional tools!"
